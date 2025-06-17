5bea0cc9dd7063c59843cc96dace4c887e72e99a
ZaRi1l <rbxo0913@gmail.com>
2025-06-17 12:06:47 +0900

    모든 요청 api에 쿠키 허용

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+// 모든 API 요청에 withCredentials: true 설정
+axios.defaults.withCredentials = true;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
