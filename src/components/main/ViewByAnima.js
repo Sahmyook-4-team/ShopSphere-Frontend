9b6a4e76af1f99fec21f3795a76fdc7fb1108e04
kimdonghyun296 <2q3w4e5r6t7y@naver.com>
2025-06-18 17:26:51 +0900

    kimd viewbyanima.js

--- src/components/main/ViewByAnima.js
+++ src/components/main/ViewByAnima.js
@@ -5,37 +5,37 @@ export const ViewByAnima = () => {
   return (
     <div className={styles.viewByAnima}>
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.bagImage}`} />
         <span>가방 · 지갑</span>
       </div>
 
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.shirtImage}`} />
         <span>셔츠</span>
       </div>
 
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.hatImage}`} />
         <span>모자</span>
       </div>
 
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.trainingImage}`} />
         <span>트레이닝</span>
       </div>
 
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.digitalImage}`} />
         <span>디지털</span>
       </div>
 
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.fashionImage}`} />
         <span>패션소품</span>
       </div>
 
       <div className={styles.roundButton}>
-        <div className={styles.roundIcon}></div>
+        <div className={`${styles.roundIcon} ${styles.underwearImage}`} />
         <span>언더웨어</span>
       </div>
     </div>
